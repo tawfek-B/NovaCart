b1a6ec606dd382e639af748a44a2e5b3473958df
Grombrindle <everythingiswronj@gmail.com>
2024-11-29 14:53:30 +0300

    nothing
    added firstname,lastname,location

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,13 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('firstname');
+            $table->string('lastname');
             $table->integer('number')->unique();
             $table->boolean('admin');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('logo');
+            $table->string('location');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
